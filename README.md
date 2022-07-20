41b51b1f1b6bc0f79ed0b688189a18faf30b68ec
rodsonnazario <rodson.nazario@gmail.com>
2022-07-19 23:50:09 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+## Aula 01
+
+Nesta primeira aula, vamos construir uma aplicação do zero para consumir a API do IMDb e exibir os filmes mais populares, destacando seus pôsteres e visualizando sua classificação... Tudo isso sem usar nenhuma biblioteca externa!
+
+## Material complementar
+
+- Artigo na Alura [O que é JSON?](https://www.alura.com.br/artigos/o-que-e-json).
+
+- Artigo na Alura sobre como [pintar o terminal, com tabelinha de cores e negrito](https://www.alura.com.br/artigos/decorando-terminal-cores-emojis).
+
+- Artigo na Alura sobre como usar [VS Code para codar em Java](https://www.alura.com.br/artigos/desenvolvendo-aplicacoes-java-vs-code).
---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão http e pegar os dados do endpoint que lista os top 250 filmes
+        var url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        var endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        var response = client.send(request, BodyHandlers.ofString());
+        var body = response.body();        
+
+        // converter o jason recebido em uma lista
+        var listaDeFilmes = JsonParser.parse(body);
+
+        // manipular e exibir os dados
+        listaDeFilmes.forEach(filme -> {            
+            System.out.println("Posição: " + filme.get("rank"));
+            System.out.println("Título: " + filme.get("title"));
+            System.out.println("Poster: " + filme.get("image"));
+            System.out.println("Ano: " + filme.get("year"));
+            System.out.println("Classificação: " + filme.get("imDbRating"));       
+            System.out.println();     
+        });
+    }
+}
